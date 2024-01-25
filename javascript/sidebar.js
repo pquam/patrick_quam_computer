5e49e37deaf4e11092a972f2c83b2406c7a17a94
Patrick Quam <pquam@hotmail.com>
2024-01-25 11:27:03 -0800

    add changes from htx_quam_computer repo

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,20 +1,14 @@
 function toggleNav() {
   const sidebar = document.querySelector('.side-navigation');
-  const tab = document.querySelector('.nav-toggle-btn');
   if ((window.innerWidth >= 1500) && (sidebar.style.display === 'none' || sidebar.style.display === '')) { 
     sidebar.style.display = 'block'; 
-    tab.style.left = '175px'; 
   } else if (sidebar.style.display === 'none' || sidebar.style.display === '') {
       sidebar.style.display = 'block';
-      tab.style.left = '350px';
   } else {
      sidebar.style.display = 'none';
-     tab.style.left = '-70px';
   }
-
   
 }
-
 
 //if the window width is increased, return the sidebar to normal
 window.addEventListener('resize', function() {
